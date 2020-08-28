90348dbfa66c080e968bf8e70abd023d465a09cb
Blard Justin <justin.blard@epitech.eu>
2020-08-28 19:58:17 +0200

    [RW] EventHandler
    Now all of the event are set in a separate file

--- src/core.c
+++ src/core.c
@@ -6,10 +6,10 @@
 */
 
 #include <SFML/Window/VideoMode.h>
-#include <SFML/Graphics/RenderWindow.h>
 #include <SFML/Graphics/Sprite.h>
 #include <SFML/Audio/Sound.h>
 #include "ressources.h"
+#include "event.h"
 
 sfSprite *setup_sprite(ressources_t *res)
 {
@@ -32,48 +32,19 @@ sfSound *setup_sound(ressources_t *res)
     return (sp);
 }
 
-void music_event(sfEvent event, sfMusic *obj)
-{
-    if (event.type == sfEvtKeyPressed && event.key.code == sfKeySpace) {
-        if (sfMusic_getStatus(obj) == sfStopped)
-            sfMusic_play(obj);
-        else if (sfMusic_getStatus(obj) == sfPlaying)
-            sfMusic_stop(obj);
-    }
-}
-
-void quit_event(sfEvent event, sfMusic *obj)
-{
-    if (event.type == sfEvtClosed)
-        sfRenderWindow_close(obj);
-}
-
-void sound_play_event(sfEvent event, sfSound *obj)
-{
-    if (event.type == sfEvtMouseButtonPressed)
-        sfSound_play(obj);
-}
-
 int core(int argc, char const *argv[])
 {
     ressources_t *res = load_ressources();
-    sfEvent event;
     sfSound *sound = setup_sound(res);
     sfSprite *sprite = setup_sprite(res);
     sfVideoMode mode = {1280, 720, 32};
     sfRenderWindow *rwin = sfRenderWindow_create(mode, "Name", sfDefaultStyle, NULL);
-    void (*event_list[3])(sfEvent e, void *item) = {&quit_event, &sound_play_event, &music_event};
-    void *object_list[3] = {rwin, sound, res->music[m_tuba_knight_boss]};
 
     sfRenderWindow_setFramerateLimit(rwin, 60);
     sfRenderWindow_setVerticalSyncEnabled(rwin, sfTrue);
     while (res != NULL && rwin != NULL && sfRenderWindow_isOpen(rwin)) {
         sfRenderWindow_clear(rwin, sfBlack);
-        while (sfRenderWindow_pollEvent(rwin, &event)) {
-            for (size_t i = 0; i < 3; i++) {
-                event_list[i](event, object_list[i]);
-            }
-        }
+        handle_event(rwin, res, sound);
         sfRenderWindow_drawSprite(rwin, sprite, NULL);
         sfRenderWindow_display(rwin);
     }
